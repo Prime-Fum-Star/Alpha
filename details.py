5547d312a2a448d27a1844d5a92c5b06d2b4a31d
Prime-Fum-Star <primefuntv881@gmail.com>
2025-02-23 12:27:11 +0530

    Delete details.py

--- details.py
+++
@@ -1,7 +0,0 @@
-api_id = 26600589
-api_hash = "1e06c253a61c1f4f548cbb9ebc09644d"
-bot_token = "5940119184:AAHKEw7V7zGVZvwV4T4RCgmZKfR7lSGKNNE"
-auth_users = [6134859761,-1001757538109]
-sudo_user = 6134859761
-log_channel = -1001852610640
-txt_channel = -1001852610640
